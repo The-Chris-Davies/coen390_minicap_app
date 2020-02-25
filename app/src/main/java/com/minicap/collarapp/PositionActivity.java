69b00e519e722c7017b45c579276a37382fb0e32
Chris Davies <chriskd662@gmail.com>
2020-02-25 18:25:39 -0500

    temporary position layout completed

--- app/src/main/java/com/minicap/collarapp/PositionActivity.java
+++ app/src/main/java/com/minicap/collarapp/PositionActivity.java
@@ -3,12 +3,22 @@ package com.minicap.collarapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PositionActivity extends AppCompatActivity {
 
+    private TextView latitudeVal;
+    private TextView longitudeVal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //initialize views
+        latitudeVal = findViewById(R.id.positionActivityLatitudeValue);
+        longitudeVal = findViewById(R.id.positionActivityLongitudeValue);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_position);
     }
+
 }
