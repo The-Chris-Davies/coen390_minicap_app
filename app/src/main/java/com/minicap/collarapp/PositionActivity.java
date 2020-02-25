99503ee49016c2ded2a3e4a90466bede0004e196
Chris Davies <chriskd662@gmail.com>
2020-02-25 17:39:23 -0500

    created position activity

---
+++ app/src/main/java/com/minicap/collarapp/PositionActivity.java
@@ -0,0 +1,14 @@
+package com.minicap.collarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PositionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_position);
+    }
+}
