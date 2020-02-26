81238ced55d285857f1a8f5c851cac228e7c31cb
Chris Davies <chriskd662@gmail.com>
2020-02-26 14:40:49 -0500

    Created position class

---
+++ app/src/main/java/com/minicap/collarapp/Position.java
@@ -0,0 +1,72 @@
+package com.minicap.collarapp;
+
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.GeoPoint;
+
+public class Position implements Comparable<Position> {
+
+    GeoPoint value;
+    Timestamp timestamp;
+
+    public Position(GeoPoint value, Timestamp timestamp) {
+        this.value = value;
+        this.timestamp = timestamp;
+    }
+
+    public Position() {
+        this.value = new GeoPoint(0.0,0.0);
+        this.timestamp = Timestamp.now();
+    }
+
+    public Position(double lat, double lon) {
+        this.value = new GeoPoint(lat, lon);
+        this.timestamp = Timestamp.now();
+    }
+
+    public Position(double lat, double lon, Timestamp timestamp) {
+        this.value = new GeoPoint(lat, lon);
+        this.timestamp = timestamp;
+    }
+
+    public GeoPoint getValue() {
+        return value;
+    }
+
+    public void setValue(GeoPoint value) {
+        this.value = value;
+    }
+
+    public double getLatitude() {
+        return value.getLatitude();
+    }
+
+    public double getLongitude() {
+        return value.getLongitude();
+    }
+
+    public void setLatitude(double lat) {
+        value = new GeoPoint(lat,this.value.getLongitude());
+    }
+
+    public void setLongitude(double lon) {
+        value = new GeoPoint(this.value.getLatitude(), lon);
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setCurrent() {
+        //update the timestamp to the current time
+        this.timestamp = Timestamp.now();
+    }
+
+    public int compareTo(Position that) {
+        //sort based on timestamp
+        return this.timestamp.compareTo(that.timestamp);
+    }
+}
