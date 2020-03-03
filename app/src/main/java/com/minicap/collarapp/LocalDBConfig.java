1bedc80ac5ea1f6083730d7d48b90d2bad80a0a4
Chris Davies <chriskd662@gmail.com>
2020-03-03 12:38:44 -0500

    moved DB Config to separate file

---
+++ app/src/main/java/com/minicap/collarapp/LocalDBConfig.java
@@ -0,0 +1,31 @@
+package com.minicap.collarapp;
+
+public class LocalDBConfig {
+    public static final String DATABASE_NAME = "collar-db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String DOG_TABLE_NAME = "dog";
+    public static final String DOG_COLUMN_ID = "dogid";
+    public static final String DOG_COLUMN_TITLE = "dogname";
+    public static final String DOG_COLUMN_BATTERY = "batterylevel";
+
+    public static final String POSITION_TABLE_NAME = "position";
+    public static final String POSITION_COLUMN_ID = "positionid";
+    public static final String POSITION_COLUMN_DOG = "positiondog";
+    public static final String POSITION_COLUMN_TIMESTAMP = "positiontime";
+    public static final String POSITION_COLUMN_LATITUDE = "positionlat";
+    public static final String POSITION_COLUMN_LONGITUDE = "positionlon";
+
+    public static final String HEARTRATE_TABLE_NAME = "heartrate";
+    public static final String HEARTRATE_COLUMN_ID = "heartrateid";
+    public static final String HEARTRATE_COLUMN_DOG = "heartratedog";
+    public static final String HEARTRATE_COLUMN_TIMESTAMP = "heartratetime";
+    public static final String HEARTRATE_COLUMN_VALUE = "heartrateval";
+
+    public static final String TEMPERATURE_TABLE_NAME = "temperature";
+    public static final String TEMPERATURE_COLUMN_ID = "temperatureid";
+    public static final String TEMPERATURE_COLUMN_DOG = "temperaturedog";
+    public static final String TEMPERATURE_COLUMN_TIMESTAMP = "temperaturetime";
+    public static final String TEMPERATURE_COLUMN_VALUE = "temperatureval";
+    public static final String TEMPERATURE_COLUMN_EXTVAL = "temperatureext";
+}
--- app/src/main/java/com/minicap/collarapp/LocalDBHelper.java
+++ app/src/main/java/com/minicap/collarapp/LocalDBHelper.java
@@ -51,7 +51,7 @@ public class LocalDBHelper extends SQLiteOpenHelper {
                 LocalDBConfig.POSITION_COLUMN_LATITUDE + " REAL, " +
                 LocalDBConfig.POSITION_COLUMN_LONGITUDE + " REAL " +
                 ")";
-        
+
 
         //write the command to the log
         Log.d(TAG, CREATE_TABLE_POSITION);
@@ -99,33 +99,5 @@ public class LocalDBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int ii) {
         //only called on database layout change
     }
-
-private class LocalDBConfig {
-    public static final String DATABASE_NAME = "collar-db";
-    public static final int DATABASE_VERSION = 1;
-
-    public static final String DOG_TABLE_NAME = "dog";
-    public static final String DOG_COLUMN_ID = "dogid";
-    public static final String DOG_COLUMN_TITLE = "dogname";
-    public static final String DOG_COLUMN_BATTERY = "batterylevel";
-
-    public static final String POSITION_TABLE_NAME = "position";
-    public static final String POSITION_COLUMN_ID = "positionid";
-    public static final String POSITION_COLUMN_DOG = "positiondog";
-    public static final String POSITION_COLUMN_TIMESTAMP = "positiontime";
-    public static final String POSITION_COLUMN_LATITUDE = "positionlat";
-    public static final String POSITION_COLUMN_LONGITUDE = "positionlon";
-
-    public static final String HEARTRATE_TABLE_NAME = "heartrate";
-    public static final String HEARTRATE_COLUMN_ID = "heartrateid";
-    public static final String HEARTRATE_COLUMN_DOG = "heartratedog";
-    public static final String HEARTRATE_COLUMN_TIMESTAMP = "heartratetime";
-    public static final String HEARTRATE_COLUMN_VALUE = "heartrateval";
-
-    public static final String TEMPERATURE_TABLE_NAME = "temperature";
-    public static final String TEMPERATURE_COLUMN_ID = "temperatureid";
-    public static final String TEMPERATURE_COLUMN_DOG = "temperaturedog";
-    public static final String TEMPERATURE_COLUMN_TIMESTAMP = "temperaturetime";
-    public static final String TEMPERATURE_COLUMN_VALUE = "temperatureval";
-    public static final String TEMPERATURE_COLUMN_EXTVAL = "temperatureext";
 }
+
